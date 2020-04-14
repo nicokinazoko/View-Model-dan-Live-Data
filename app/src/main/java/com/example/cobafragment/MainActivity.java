beaebb95f5717dca0177bfcd91ca606f73324764
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2020-04-14 19:49:43 +0700

    Tampilan Fragment Done
    Masih membuat Fragment

--- app/src/main/java/com/example/cobafragment/MainActivity.java
+++ app/src/main/java/com/example/cobafragment/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.cobafragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectedFragment           =   new MovieFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView                    =   findViewById(R.id.activitymain_bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(selectedFragment);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.menu_bottomnav_movie:
+                selectedFragment                =   new MovieFragment();
+                loadFragment(selectedFragment);
+                break;
+            case R.id.menu_bottomnav_tvshow:
+                selectedFragment                =   new TvshowFragment();
+                loadFragment(selectedFragment);
+                break;
+
+        }
+        return loadFragment(selectedFragment);
+    }
+
+    private boolean loadFragment(Fragment selectedFragment) {
+
+        if(selectedFragment != null)
+        {
+            getSupportFragmentManager().
+                    beginTransaction().
+                    replace(R.id.activitymain_fragmentcontainer,selectedFragment).
+                    commit();
+            return  true;
+        }
+        return false;
+
     }
 }
